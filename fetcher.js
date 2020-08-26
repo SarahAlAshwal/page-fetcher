aaa9ba9f4bcf8ba17e6074e7105ba313893c1f02
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-26 11:05:27 +0000

    adding the stretch feature and correcting the eslint errors

--- fetcher.js
+++ fetcher.js
@@ -2,7 +2,25 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+const r1 = readline.createInterface({
+  input: process.stdin ,
+  output: process.stdout
+});
 
+const write = (text) => {
+  fs.writeFile(filePath, text , function(err) {
+    if (err) {
+      throw err;
+    } else {
+      let stats = fs.statSync(filePath);
+      let fileSizeInB = stats["size"];
+      console.log(`Downloaded and saved ${fileSizeInB} bytes to ${filePath} `);
+      process.exit();
+    }
+ 
+  });
+};
 
 request(url,(error, response, body) => {
   if (error) {
@@ -12,23 +30,20 @@ request(url,(error, response, body) => {
  
     fs.access(filePath, (err) => {
       if (err) {
-          console.log("The file does not exist.");
+        //console.log("The file does not exist.");
+        write(text);
       } else {
-        fs.writeFile(filePath, text , function (err) {
-          if (err) {
-            throw err;
-          } else {
+        r1.question('The file already exists if you want to overwite pres y then ENTER:  ',(answer) => {
+          if (answer === 'y') {
+            write(text);
             
-              let stats = fs.statSync(filePath);
-              let fileSizeInB = stats["size"];
-              console.log(`Downloaded and saved ${fileSizeInB} bytes to ${filePath} `);
+          } else {
+            process.exit();
           }
-         
         });
       }
     });
   }
  
-  
 });
 
